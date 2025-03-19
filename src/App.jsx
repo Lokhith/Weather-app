b8a46b37ac4530221d4e1920c328ecb3ddd41abd
Lokhith <lokhithaswa6@gmail.com>
2025-03-19 16:52:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+import { useState } from 'react';
+import './App.css';
+function App() {
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const apiKey = 'c14957fe58681790fb180bc21d254ee6';
+
+  const fetchWeather = async () => {
+    if (!city) {
+      setError('Please enter a city name');
+      return;
+    }
+
+    setError('');
+    setWeather(null);
+    setLoading(true);
+
+    setTimeout(async () => {
+      try {
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+        );
+        const data = await response.json();
+
+        if (data.cod !== 200) {
+          setError('City not found');
+          setLoading(false);
+          return;
+        }
+
+        setWeather(data);
+      } catch {
+        setError('Failed to fetch weather');
+      } finally {
+        setLoading(false);
+      }
+    }, 2000);
+  };
+
+  return (
+    <div className="app-container">
+      <div className="left-panel">
+        <h1>Weather App</h1>
+        <div className="search">
+          <input
+            type="text"
+            placeholder="Enter city name"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <button onClick={fetchWeather}>Search</button>
+        </div>
+        {error && <div className="error">{error}</div>}
+      </div>
+
+      <div className="right-panel">
+        {loading ? (
+          <div className="spinner-wrapper">
+            <img src="../src/assets/wheel-logo.jpg" alt="Loading..." className="spinner" />
+          </div>
+        ) : weather ? (
+          <div className="weather-box">
+            <img
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt="weather-icon"
+            />
+            <h2>{weather.name}</h2>
+            <h3>{weather.main.temp}°C</h3>
+            <p>{weather.weather[0].description}</p>
+            <p>Humidity: {weather.main.humidity}%</p>
+            <p>Wind: {weather.wind.speed} km/h</p>
+          </div>
+        ) : (
+          <div className="placeholder-text">Search a city to see weather details</div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
